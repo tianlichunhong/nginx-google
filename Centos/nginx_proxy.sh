7edd9ee95bf9d3580b46a97d94c60113c058bdf1
tianlichunhong <tianlichunhong@gmail.com>
2018-10-03 10:13:41 +0800

    更新因升级nginx 1.15.5 Bug
    更新因升级nginx 1.15.5 Bug

--- Centos/nginx_proxy.sh
+++ Centos/nginx_proxy.sh
@@ -54,14 +54,14 @@ else
 	yum install -y gcc gcc-c++ make automake
 fi
 cd /usr/src
-#3.download  nginx-1.13.9
+#3.download  nginx-1.15.5
 wget -N http://nginx.org/download/nginx-1.15.5.tar.gz
 tar -zxvf nginx-1.15.5.tar.gz
 #4.download  ngx_http_substitutions_filter_module
 wget -N --no-check-certificate https://raw.githubusercontent.com/tianlichunhong/nginx-google/master/ngx_http_substitutions_filter_module.tar.gz
 tar -zxvf ngx_http_substitutions_filter_module.tar.gz
 #5.configure for nginx
-cd /usr/src/nginx-1.13.9
+cd /usr/src/nginx-1.15.5
 mkdir -p /var/lib/nginx/body
 ./configure --prefix=/etc/nginx --conf-path=/etc/nginx/nginx.conf --error-log-path=/var/log/nginx/error.log --http-client-body-temp-path=/var/lib/nginx/body --http-fastcgi-temp-path=/var/lib/nginx/fastcgi --http-log-path=/var/log/nginx/access.log --http-proxy-temp-path=/var/lib/nginx/proxy --http-scgi-temp-path=/var/lib/nginx/scgi --http-uwsgi-temp-path=/var/lib/nginx/uwsgi --lock-path=/var/lock/nginx.lock --pid-path=/var/run/nginx.pid --with-pcre-jit --with-debug --with-http_addition_module --with-http_dav_module  --with-http_gzip_static_module  --with-http_realip_module --with-http_stub_status_module --with-http_ssl_module --with-http_sub_module --with-ipv6 --with-sha1=/usr/include/openssl --with-md5=/usr/include/openssl --with-mail --with-mail_ssl_module --with-http_sub_module --add-module=/usr/src/ngx_http_substitutions_filter_module
 #6.make && make install for nginx
